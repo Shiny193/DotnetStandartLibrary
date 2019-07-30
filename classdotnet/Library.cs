3ac00c4ea6775e858299b3f8f7d481bcba837464
Gera <shinygold.gera@thermofisher.com>
2019-07-30 09:40:56 +0530

    first commit

---
+++ classdotnet/Library.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace classdotnet
+{
+    public class Library<T>
+    {
+        double a, b, temp;
+        public T Add(T value1, T value2)
+        {
+            
+            a = Convert.ToDouble(value1);
+            b = Convert.ToDouble(value2);
+            temp = a + b;
+            return (T)Convert.ChangeType(temp, typeof(T));
+        }
+        public T Sub(T value1, T value2)
+        {
+           
+            a = Convert.ToDouble(value1);
+            b = Convert.ToDouble(value2);
+            temp = a - b;
+            return (T)Convert.ChangeType(temp, typeof(T));
+        }
+        public T Mul(T value1, T value2)
+        {
+           
+            a = Convert.ToDouble(value1);
+            b = Convert.ToDouble(value2);
+            temp = a * b;
+            return (T)Convert.ChangeType(temp, typeof(T));
+        }
+        public double Div(T value1, T value2)
+        {
+
+           
+            a = Convert.ToDouble(value1);
+            b = Convert.ToDouble(value2);
+            if (b == 0)
+            {
+                Console.WriteLine("Denominator can't be zero_ DivideByZeroException");
+                return 0;
+            }
+            else
+            {
+                return (a / b);
+            }
+        }
+            
+        public double Addninp(params double[] numbers)
+        { 
+                double sum = 0;
+                foreach (double i in numbers)
+                {
+                    sum += i;
+                }
+            return sum;
+             
+        }
+    } 
+
+}
+
+    
+
---
+++ framework/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            
+            classdotnet.Library<int> obj = new classdotnet.Library<int>();
+
+            Console.WriteLine(".NET Framework.....Enter two integers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+             
+            Console.WriteLine("Addition: " + obj.Add(a, b));
+            Console.WriteLine("Subtraction: " + obj.Sub(a, b));
+            Console.WriteLine("Multiplication: " + obj.Mul(a, b));
+            Console.WriteLine("Division: " + obj.Div(a, b));
+            Console.WriteLine(obj.Addninp(12, 23, 76, 45,15,16));
+
+            double d, c;
+            classdotnet.Library<double> obj1 = new classdotnet.Library<double>();
+
+            Console.WriteLine(">NET Core.....Enter two Double Values");
+            d = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Addition: " + obj1.Add(d, c));
+            Console.WriteLine("Subtraction: " + obj1.Sub(d, c));
+            Console.WriteLine("Multiplication: " + obj1.Mul(d, c));
+            Console.WriteLine("Division: " + obj1.Div(d, c));
+            Console.WriteLine(obj.Addninp(12, 23, 76, 45, 15.6));
+
+            Console.ReadLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
